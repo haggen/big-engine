bb62edb1e6fd5432997df7000d9942b367a905b2
Arthur <arthur@corenzan.com>
2022-11-10 23:49:12 -0300

    Update debug overlays

--- src/game/debug.ts
+++ src/game/debug.ts
@@ -6,63 +6,107 @@ import { game } from ".";
 type State = {
   position: Vector;
   text: string[];
-  player: Entity<any> | undefined;
+  visible: boolean;
 };
 
-const initialState: State = {
+function format(title: string, value: string) {
+  return `${title.padEnd(12)} ${value}`;
+}
+
+export const perfOverlay = new Entity<State>({
   position: new Vector(7, 7),
   text: [],
-  player: undefined,
-};
-export const overlay = new Entity(initialState);
-game.addEntity(overlay);
+  visible: true,
+});
+game.addEntity(perfOverlay);
 
 game.addEventHandler("render", () => {
   const { renderingContext } = game;
-  const { text } = overlay.state;
+
+  if (!perfOverlay.state.visible) {
+    return;
+  }
 
   renderingContext.fillStyle = "red";
   renderingContext.font = "14px monospace";
   renderingContext.textBaseline = "top";
 
-  const position = new Vector(overlay.state.position);
-  text.forEach((line) => {
-    renderingContext.fillText(line, position.x, position.y);
+  const position = new Vector(perfOverlay.state.position);
+  perfOverlay.state.text.forEach((text) => {
+    renderingContext.fillText(text, position.x, position.y);
     position.add(0, 14);
   });
 });
 
 game.addEventHandler("update", () => {
   const { statistics } = game;
-  const { text, player } = overlay.state;
+  const { text } = perfOverlay.state;
+
+  if (game.input.F1?.fresh) {
+    console.log("F1 pressed");
+    perfOverlay.state.visible = !perfOverlay.state.visible;
+  }
 
   const time = (statistics.time / 1000).toFixed(2);
-  const renderingRate = statistics.rendering.rate.toFixed(2);
-  const renderingElapsed = statistics.rendering.elapsed.toFixed(2);
-  const simulationRate = (1000 / game.config.simulation.rate).toFixed(2);
-  const simulationElapsed = statistics.simulation.elapsed.toFixed(2);
+  const rendering = {
+    count: statistics.rendering.count,
+    rate: statistics.rendering.rate.toFixed(2),
+    elapsed: statistics.rendering.elapsed.toFixed(2),
+  };
+  const simulation = {
+    steps: statistics.simulation.steps,
+    rate: (1000 / game.config.simulation.rate).toFixed(2),
+    elapsed: statistics.simulation.elapsed.toFixed(2),
+  };
 
-  if (statistics.simulation.steps % 20 === 0) {
-    text[0] = `${"Time".padEnd(12)} ${time}s`;
-    text[1] = `${"Rendering".padEnd(
-      12
-    )} ${renderingRate}/s ${renderingElapsed}s (${statistics.rendering.count})`;
-    text[2] = `${"Simul.".padEnd(
-      12
-    )} ${simulationRate}/s ${simulationElapsed}s (${
-      statistics.simulation.steps
-    })`;
-  }
-  text[3] = `${"Input".padEnd(12)} ${Object.keys(game.input).join()}`;
+  text[0] = format("Time", `${time}s`);
+  text[1] = format(
+    "Rendering",
+    `${rendering.rate}/s ${rendering.elapsed}s (${rendering.count})`
+  );
+  text[2] = format(
+    "Simulation",
+    `${simulation.rate}/s ${simulation.elapsed}s (${simulation.steps})`
+  );
+});
 
-  if (player) {
-    text[4] = ``;
-    text[5] = `${"Player".padEnd(12)} ${player.state.position}`;
-    text[6] = `${"Direction".padEnd(12)} ${player.state.direction}`;
-    text[7] = `${"Velocity".padEnd(12)} ${player.state.velocity}`;
-    text[8] = `${"Accel.".padEnd(12)} ${player.state.acceleration}`;
-    text[9] = `${"Speed".padEnd(12)} ${player.state.speed.toFixed(2)}`;
-  } else {
-    overlay.state.player = game.findEntity("player");
+// ---
+
+export const inputOverlay = new Entity<State>({
+  position: new Vector(game.canvasElement.width - 200, 7),
+  text: [],
+  visible: true,
+});
+game.addEntity(inputOverlay);
+
+game.addEventHandler("render", () => {
+  const { renderingContext } = game;
+
+  if (!inputOverlay.state.visible) {
+    return;
   }
+
+  renderingContext.fillStyle = "red";
+  renderingContext.font = "14px monospace";
+  renderingContext.textBaseline = "top";
+
+  const position = new Vector(inputOverlay.state.position);
+  inputOverlay.state.text.forEach((text) => {
+    renderingContext.fillText(text, position.x, position.y);
+    position.add(0, 14);
+  });
+});
+
+game.addEventHandler("update", () => {
+  if (game.input.F1?.fresh) {
+    inputOverlay.state.visible = !inputOverlay.state.visible;
+  }
+
+  inputOverlay.state.text = [];
+
+  Object.values(game.input).forEach((state, index) => {
+    inputOverlay.state.text[index] = `${state.key} ${
+      state.fresh
+    } ${state.duration.toFixed(2)}`;
+  });
 });
